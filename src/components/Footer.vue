a8c12b72f941f2393776f7b614b60f41ac155fc1
Charlie Zhuo <charliezm88@outlook.com>
2024-08-12 17:04:12 +0800

    将主页、footer和根模板改回realworld的标准

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,11 @@
 <template>
   <footer>
     <div class="container">
-      <AppLink
-        name="global-feed"
-        class="logo-font"
-      >
-        conduit
-      </AppLink>
+      <a href="/" class="logo-font">conduit</a>
       <span class="attribution">
-        An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
-      </span>
-    </div>
-    <div class="container">
-      <span
-        class="attribution"
-        style="margin: 0"
-      >
-        <a href="https://github.com/mutoe/vue3-realworld-example-app">Real world app</a> implementation using Vue v3 with Typescript and Composition Api
+        An interactive learning project from
+        <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed
+        under MIT.
       </span>
     </div>
   </footer>
