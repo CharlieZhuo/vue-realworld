1a516ecf5aca668af574e3c3fd0fd5b6cfc708b8
Charlie Zhuo <charliezm88@outlook.com>
2024-08-17 11:11:42 +0800

    实现标签列表TagList 组件

---
+++ src/components/TagList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="col-md-3">
+    <div class="sidebar">
+      <p>Popular Tags</p>
+      <div class="tag-list">
+        <RouterLink
+          v-for="tag in props.tags"
+          :key="tag"
+          :to="{ name: 'tag-feeds', params: { tag } }"
+          class="tag-pill tag-default"
+        >
+          {{ tag }}
+        </RouterLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  tags: string[];
+}>();
+</script>
