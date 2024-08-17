b6540b6a5d7dcc175c90d62f3ad373fde930bcf2
Charlie Zhuo <charliezm88@outlook.com>
2024-08-17 11:11:11 +0800

    实现分页组件

---
+++ src/components/Pagination.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="pagination" v-if="pageCount > 1">
+    <li
+      class="page-item"
+      v-for="i in pageCount"
+      :key="i"
+      :class="{ active: i === prop.currentPage }"
+    >
+      <a class="page-link" @click="emit('pageChange', i)">{{ i }}</a>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+const prop = defineProps<{
+  total: number;
+  pageSize: number;
+  currentPage: number;
+}>();
+
+const emit = defineEmits<{
+  (e: "pageChange", to: number): void;
+}>();
+
+const pageCount = Math.ceil(prop.total / prop.pageSize);
+</script>
