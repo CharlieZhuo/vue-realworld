cd452033ec281eeb1d25bf40504f4ab288f71f4b
Charlie Zhuo <charliezm88@outlook.com>
2024-08-17 16:53:24 +0800

    实现文章预览组件

---
+++ src/components/ArticlePreview.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="article-preview">
+    <div class="article-meta">
+      <RouterLink
+        :to="{
+          name: 'profile',
+          params: { username: article.author.username },
+        }"
+      >
+        <img :src="article.author.image" />
+      </RouterLink>
+      <div class="info">
+        <RouterLink
+          :to="{
+            name: 'profile',
+            params: { username: article.author.username },
+          }"
+        >
+          {{ article.author.username }}
+        </RouterLink>
+        <span class="date">{{ article.createdAt }}</span>
+      </div>
+      <button class="btn btn-outline-primary btn-sm pull-xs-right">
+        <i class="ion-heart"></i> {{ article.favoritesCount }}
+      </button>
+    </div>
+    <RouterLink
+      :to="{ name: 'article', params: { id: article.slug } }"
+      class="preview-link"
+    >
+      <h1>{{ article.title }}</h1>
+      <p>{{ article.description }}</p>
+      <span>Read more...</span>
+      <ul class="tag-list">
+        <li
+          v-for="tag in article.tagList"
+          class="tag-default tag-pill tag-outline"
+        >
+          {{ tag }}
+        </li>
+      </ul>
+    </RouterLink>
+  </div>
+</template>
+<script setup lang="ts">
+import { components } from "../api/schema";
+
+const {article} = defineProps<{ article: components["schemas"]["Article"] }>();
+</script>
